fdb8f064f3486c7cbe94846801599090ff0322a9
Alexey Melezhik <melezhik@gmail.com>
2021-09-16 16:50:25 +0000

    add project utility

---
+++ .tom/add-project.pl6
@@ -0,0 +1,8 @@
+task-run ".tom/tasks/add-project", %(
+  project => "Red",
+  url => "https://github.com/FCO/Red",
+  language => "raku",
+  description => "Red - A WiP ORM for Raku",
+  category => "databases",
+  
+);
---
+++ .tom/tasks/add-project/task.bash
@@ -0,0 +1,27 @@
+#!bash
+
+project=$(config project)
+description=$(config description)
+url=$(config url)
+language=$(config language)
+category=$(config category)
+
+
+mkdir -p ~/.mbf/projects/$project
+
+mkdir -p ~/.mbf/projects/$project/reviews
+mkdir -p ~/.mbf/projects/$project/reviews/data
+mkdir -p ~/.mbf/projects/$project/reviews/points
+
+mkdir -p ~/.mbf/projects/$project/ups
+
+
+cat << HERE > ~/.mbf/projects/$project/meta.json
+{
+  "project" : "$project",
+  "description" : "$description",
+  "category" : "$category",
+  "language" : "$language",
+  "url" : "$url"
+}
+HERE
