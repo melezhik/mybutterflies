7070e73f153322aa92c690d3dd11029cb36227cc
Alexey Melezhik <melezhik@gmail.com>
2021-12-29 00:39:43 +0000

    installer

---
+++ install.sh
@@ -0,0 +1,16 @@
+zef install --/test Sparrowdo Sparky
+zef install --/test JSON::Tiny Text::Markdown
+zef install --/test YAMLish JSON:Fast
+zef install --/test HTML::Strip
+zef install --/test Tomtit
+
+export OAUTH_CLIENT_SECRET=*****************
+export OAUTH_CLIENT_ID=*********************
+export OAUTH_STATE=mybfANDraku
+
+sudo apt-get install libperlio-eol-perl
+sudo apt-get install git nano wget curl
+sudo apt-get install libssl-dev
+sudo apt-get install make gcc
+sudo apt install python3-certbot-nginx
+sudo apt-get install hunspell
